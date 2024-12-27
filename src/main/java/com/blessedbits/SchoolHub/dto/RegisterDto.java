548b5fcf58a2d17dbf5dbe782057a1852bfc9c17
volodymyr-rievkov <volodymyr.rievkov.shi.2023@lpnu.ua>
2024-12-27 18:56:22 +0200

    Added validation to email filed

--- src/main/java/com/blessedbits/SchoolHub/dto/RegisterDto.java
+++ src/main/java/com/blessedbits/SchoolHub/dto/RegisterDto.java
@@ -1,10 +1,12 @@
 package com.blessedbits.SchoolHub.dto;
+import jakarta.validation.constraints.Email;
 
 import lombok.Data;
 
 @Data
 public class RegisterDto {
     private String username;
+    @Email(message = "Invalid email format")
     private String email;
     private String password;
 }
