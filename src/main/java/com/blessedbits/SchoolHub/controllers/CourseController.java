c6dddea81285ba75894a9bd1eb645171782ea8a3
oleksandr-ilnytskyi <146804152+oleksandr-ilnytskyi@users.noreply.github.com>
2025-01-03 21:59:07 +0200

    Added relations and assignments/materials functionality

--- src/main/java/com/blessedbits/SchoolHub/controllers/CourseController.java
+++ src/main/java/com/blessedbits/SchoolHub/controllers/CourseController.java
@@ -1,11 +1,13 @@
 package com.blessedbits.SchoolHub.controllers;
 
+import com.blessedbits.SchoolHub.dto.CreateAssignmentDto;
 import com.blessedbits.SchoolHub.dto.CreateCourseDto;
+import com.blessedbits.SchoolHub.dto.CreateMaterialDto;
 import com.blessedbits.SchoolHub.dto.CreateModuleDto;
-import com.blessedbits.SchoolHub.models.Course;
-import com.blessedbits.SchoolHub.models.ModuleEntity;
-import com.blessedbits.SchoolHub.models.UserEntity;
+import com.blessedbits.SchoolHub.models.*;
+import com.blessedbits.SchoolHub.repositories.AssignmentRepository;
 import com.blessedbits.SchoolHub.repositories.CourseRepository;
+import com.blessedbits.SchoolHub.repositories.MaterialRepository;
 import com.blessedbits.SchoolHub.repositories.ModuleRepository;
 import com.blessedbits.SchoolHub.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,13 +24,18 @@ public class CourseController {
     private final CourseRepository courseRepository;
     private final UserService userService;
     private final ModuleRepository moduleRepository;
+    private final MaterialRepository materialRepository;
+    private final AssignmentRepository assignmentRepository;
 
     @Autowired
     public CourseController(CourseRepository courseRepository,
-                            UserService userService, ModuleRepository moduleRepository) {
+                            UserService userService, ModuleRepository moduleRepository,
+                            MaterialRepository materialRepository, AssignmentRepository assignmentRepository) {
         this.courseRepository = courseRepository;
         this.userService = userService;
         this.moduleRepository = moduleRepository;
+        this.materialRepository = materialRepository;
+        this.assignmentRepository = assignmentRepository;
     }
 
     @GetMapping("/")
@@ -43,7 +50,7 @@ public class CourseController {
         UserEntity user = userService.getUserFromHeader(authorizationHeader);
         Course course = new Course();
         course.setName(courseDto.getName());
-        course.setSchool(user.getUserClass().getSchool());
+        course.setSchool(user.getSchool());
         try {
             courseRepository.save(course);
             return new ResponseEntity<>("Course created", HttpStatus.CREATED);
@@ -66,7 +73,7 @@ public class CourseController {
             @RequestHeader("Authorization") String authorizationHeader) {
         UserEntity user = userService.getUserFromHeader(authorizationHeader);
         Optional<Course> courseOptional = courseRepository
-                .findCourseByNameAndSchoolId(courseName, user.getUserClass().getSchool().getId());
+                .findCourseByNameAndSchoolId(courseName, user.getSchool().getId());
         if (courseOptional.isEmpty()) {
             return new ResponseEntity<>("No course with that name was found in your school",
                     HttpStatus.NOT_FOUND);
@@ -82,4 +89,64 @@ public class CourseController {
             return new ResponseEntity<>("Couldn't create module", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @PostMapping("/{courseName}/modules/{moduleName}/new-material")
+    public ResponseEntity<String> createMaterial(
+            @PathVariable String courseName,
+            @PathVariable String moduleName,
+            @RequestBody CreateMaterialDto materialDto,
+            @RequestHeader("Authorization") String authorizationHeader
+    ) {
+        UserEntity user = userService.getUserFromHeader(authorizationHeader);
+        Material material = new Material();
+        material.setTitle(materialDto.getTitle());
+        material.setDescription(materialDto.getDescription());
+        material.setUrl(materialDto.getUrl());
+        Optional<ModuleEntity> moduleOptional = moduleRepository.findByNameAndCourseNameAndCourse_SchoolId(
+                moduleName, courseName, user.getSchool().getId());
+        if (moduleOptional.isEmpty()) {
+            return new ResponseEntity<>("Module for given course was not found," +
+                    " or course with that name was not found in your school", HttpStatus.NOT_FOUND);
+        }
+        ModuleEntity module = moduleOptional.get();
+        material.setModule(module);
+        try {
+            materialRepository.save(material);
+            return new ResponseEntity<>("Material created", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Couldn't create material", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/{courseName}/modules/{moduleName}/new-assignment")
+    public ResponseEntity<String> createAssignment(
+            @PathVariable String courseName,
+            @PathVariable String moduleName,
+            @RequestBody CreateAssignmentDto assignmentDto,
+            @RequestHeader("Authorization") String authorizationHeader
+    ) {
+        UserEntity user = userService.getUserFromHeader(authorizationHeader);
+        Assignment assignment = new Assignment();
+        assignment.setTitle(assignmentDto.getTitle());
+        assignment.setDescription(assignmentDto.getDescription());
+        assignment.setUrl(assignmentDto.getUrl());
+        assignment.setDueDate(assignmentDto.getDueDate());
+        Optional<ModuleEntity> moduleOptional = moduleRepository.findByNameAndCourseNameAndCourse_SchoolId(
+                moduleName, courseName, user.getSchool().getId()
+        );
+        if (moduleOptional.isEmpty()) {
+            return new ResponseEntity<>("Module for given course was not found," +
+                    " or course with that name was not found in your school", HttpStatus.NOT_FOUND);
+        }
+        ModuleEntity module = moduleOptional.get();
+        assignment.setModule(module);
+        try {
+            assignmentRepository.save(assignment);
+            return new ResponseEntity<>("Assignment created", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Couldn't create assignment",
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
--- src/main/java/com/blessedbits/SchoolHub/repositories/ModuleRepository.java
+++ src/main/java/com/blessedbits/SchoolHub/repositories/ModuleRepository.java
@@ -4,6 +4,9 @@ import com.blessedbits.SchoolHub.models.ModuleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ModuleRepository extends JpaRepository<ModuleEntity, Long> {
+    Optional<ModuleEntity> findByNameAndCourseNameAndCourse_SchoolId(String name, String courseName, Integer schoolId);
 }
