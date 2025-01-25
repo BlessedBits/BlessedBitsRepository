d3436b1f57e90803fcd52ef2802428b9b8af68ef
oleksandr-ilnytskyi <146804152+oleksandr-ilnytskyi@users.noreply.github.com>
2025-01-25 10:34:09 +0200

    Lowered accessToken validity and added headers for auth errors

--- src/main/java/com/blessedbits/SchoolHub/security/SecurityConstants.java
+++ src/main/java/com/blessedbits/SchoolHub/security/SecurityConstants.java
@@ -5,7 +5,7 @@ import io.jsonwebtoken.security.Keys;
 import java.security.Key;
 
 public class SecurityConstants {
-    public static final long ACCESS_TOKEN_VALIDITY = 10*60*1000;
+    public static final long ACCESS_TOKEN_VALIDITY = 1*60*1000;
     public static final long REFRESH_TOKEN_VALIDITY = 30L*60*60*24*1000;
     public static final String JWT_SECRET = "5pcGFBfkSBNpFrtgPNAl3FzpGygSwdDaLtwUQWPt";
     public static final Key SIGNING_KEY = Keys.hmacShaKeyFor(JWT_SECRET.getBytes());
