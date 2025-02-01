870afd2bc67efdef0345f5c1b534e869f556e622
volodymyr-rievkov <146222085+volodymyr-rievkov@users.noreply.github.com>
2025-02-01 20:29:10 +0200

    Get user school feature (#48)
    * Created Endpoint.
    
    * Configured Access.

--- src/main/java/com/blessedbits/SchoolHub/security/SecurityConfig.java
+++ src/main/java/com/blessedbits/SchoolHub/security/SecurityConfig.java
@@ -82,6 +82,8 @@ public class SecurityConfig {
                                 "USER", "STUDENT", "TEACHER", "SCHOOL_ADMIN", "PLATFORM_ADMIN")
                         .requestMatchers("/users/role").hasAnyRole(
                                 "USER", "STUDENT", "TEACHER", "SCHOOL_ADMIN", "PLATFORM_ADMIN")
+                        .requestMatchers("/users/school-id").hasAnyRole(
+                                "USER", "STUDENT", "TEACHER", "SCHOOL_ADMIN", "PLATFORM_ADMIN")
 
                         .requestMatchers("/schedules/new").hasRole("SCHOOL_ADMIN")
                         .requestMatchers(HttpMethod.GET, "/schedules/{id}").hasAnyRole("STUDENT", "TEACHER", "SCHOOL_ADMIN")
