a4acd0eebf9dc7eec5504ece7837cdd109380059
oleksandr-ilnytskyi <146804152+oleksandr-ilnytskyi@users.noreply.github.com>
2025-02-19 19:50:11 +0200

    Made delete course endpoint transactional

--- src/main/java/com/blessedbits/SchoolHub/controllers/CourseController.java
+++ src/main/java/com/blessedbits/SchoolHub/controllers/CourseController.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -89,6 +90,7 @@ public class CourseController {
         }
     }
 
+    @Transactional
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteCourse(
             @PathVariable Integer id,
